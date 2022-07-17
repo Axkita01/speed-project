bae93bde90efb48bcd18025b8785fcc5c4742c6e
Axkita01 <94666562+Axkita01@users.noreply.github.com>
2022-07-17 03:11:24 -0500

    adding draw feature

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,10 @@
+import eventlet
+eventlet.monkey_patch()
 from random import shuffle
-from flask import Flask, jsonify
+from flask import request
+from flask import Flask
 from flask_socketio import SocketIO
-from flask_socketio import send, emit
+from flask_socketio import emit
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins='*', logger=True)
@@ -67,19 +70,29 @@ total_cards = [
 
 
 @socketio.on('connect')
-def connect(sid):
-    print(sid)
+def connect():
     print(clients)
+    print(request.sid)
     if not clients['p1']:
-        clients['p1'] = sid
+        clients['p1'] = request.sid
     
     elif not clients['p2']:
-        clients['p2'] = sid
+        clients['p2'] = request.sid
     
     else:
         return
 
-
+@socketio.on('disconnect')
+def disconnect():
+    print(clients)
+    if request.sid == clients['p1']:
+        clients['p1'] = clients['p2']
+        clients['p2'] = None
+    
+    else:
+        clients['p2'] = None
+    print('disconnected')
+    print(clients)
 @socketio.on('push')
 def handlePlace():
     message = 'Hi!'
@@ -87,20 +100,17 @@ def handlePlace():
 
 @socketio.on('card')
 def handleCard(number):
-    print('run')
     emit('card-res', number)
 
 @socketio.on('reset')
 def reset_game():
-    cards = []
-    for card in total_cards:
-        cards.append(card)
+    cards = [card for card in total_cards]
     shuffle(cards)
-    print(cards)
     player1_cards = cards[:25]
     player2_cards = cards[25:51]
     sides = cards[51:]
-    emit('reset', player1_cards)
+    emit('reset', player1_cards, room=clients['p1'])
+    emit('reset', player2_cards, room=clients['p2'])
 
 
 if __name__ == '__main__':

0c8c0d6dc83c923864ebb8cc91a45b3d5e4314f5
Axkita01 <94666562+Axkita01@users.noreply.github.com>
2022-07-20 21:19:23 -0500

    placement fixes

--- backend/app.py
+++ backend/app.py
@@ -14,71 +14,76 @@ clients = {
     'p2': None
 }
 
-deck_tops = [[None, None], [None, None]]
+deck_tops = [['empty', 'empty'], ['empty', 'empty']]
 
 total_cards = [
-    ['one', 'red', 1],
-    ['one', 'black', 2],
-    ['two', 'red', 3],
-    ['two', 'black', 4],
-    ['three', 'red', 5],
-    ['three', 'black', 6],
-    ['four', 'red', 7],
-    ['four', 'black', 8],
-    ['five', 'red', 9],
-    ['five', 'black', 10],
-    ['six', 'red', 11],
-    ['six', 'black', 12],
-    ['seven', 'red', 13],
-    ['seven', 'black', 14],
-    ['eight', 'red', 15],
-    ['eight', 'black', 16],
-    ['nine', 'red', 17],
-    ['nine', 'black', 18],
-    ['ten', 'red', 19],
-    ['ten', 'black', 20],
-    ['eleven', 'red', 21],
-    ['eleven', 'black', 22],
-    ['twelve', 'red', 23],
-    ['twelve', 'black', 24],
-    ['thirteen', 'red', 25],
-    ['thirteen', 'black', 26],
-    ['one', 'red', 27],
-    ['one', 'black', 28],
-    ['two', 'red', 29],
-    ['two', 'black', 30],
-    ['three', 'red', 31],
-    ['three', 'black', 32],
-    ['four', 'red', 33],
-    ['four', 'black', 34],
-    ['five', 'red', 35],
-    ['five', 'black', 36],
-    ['six', 'red', 37],
-    ['six', 'black', 38],
-    ['seven', 'red', 39],
-    ['seven', 'black', 40],
-    ['eight', 'red', 41],
-    ['eight', 'black', 42],
-    ['nine', 'red', 43],
-    ['nine', 'black', 44],
-    ['ten', 'red', 45],
-    ['ten', 'black', 46],
-    ['eleven', 'red', 47],
-    ['eleven', 'black', 48],
-    ['twelve', 'red', 49],
-    ['twelve', 'black', 50],
-    ['thirteen', 'red', 51],
-    ['thirteen', 'black', 52]
+    ['1', 'red', 1],
+    ['1', 'black', 2],
+    ['2', 'red', 3],
+    ['2', 'black', 4],
+    ['3', 'red', 5],
+    ['3', 'black', 6],
+    ['4', 'red', 7],
+    ['4', 'black', 8],
+    ['5', 'red', 9],
+    ['5', 'black', 10],
+    ['6', 'red', 11],
+    ['6', 'black', 12],
+    ['7', 'red', 13],
+    ['7', 'black', 14],
+    ['8', 'red', 15],
+    ['8', 'black', 16],
+    ['9', 'red', 17],
+    ['9', 'black', 18],
+    ['10', 'red', 19],
+    ['10', 'black', 20],
+    ['11', 'red', 21],
+    ['11', 'black', 22],
+    ['12', 'red', 23],
+    ['12', 'black', 24],
+    ['13', 'red', 25],
+    ['13', 'black', 26],
+    ['1', 'red', 27],
+    ['1', 'black', 28],
+    ['2', 'red', 29],
+    ['2', 'black', 30],
+    ['3', 'red', 31],
+    ['3', 'black', 32],
+    ['4', 'red', 33],
+    ['4', 'black', 34],
+    ['5', 'red', 35],
+    ['5', 'black', 36],
+    ['6', 'red', 37],
+    ['6', 'black', 38],
+    ['7', 'red', 39],
+    ['7', 'black', 40],
+    ['8', 'red', 41],
+    ['8', 'black', 42],
+    ['9', 'red', 43],
+    ['9', 'black', 44],
+    ['10', 'red', 45],
+    ['10', 'black', 46],
+    ['11', 'red', 47],
+    ['11', 'black', 48],
+    ['12', 'red', 49],
+    ['12', 'black', 50],
+    ['13', 'red', 51],
+    ['13', 'black', 52]
 ]
 
 
 @socketio.on('connect')
 def connect():
+    if request.sid in clients.values():
+        return
+        
     if not clients['p1']:
         clients['p1'] = request.sid
     
-    if not clients['p2']:
+    elif not clients['p2']:
         clients['p2'] = request.sid
+    
+    print(clients)
 
 @socketio.on('disconnect')
 def disconnect():
@@ -88,10 +93,6 @@ def disconnect():
     
     else:
         clients['p2'] = None
-@socketio.on('push')
-def handlePlace():
-    message = 'Hi!'
-    emit('response', message)
 
 @socketio.on('card')
 def handleCard(number):
@@ -100,7 +101,7 @@ def handleCard(number):
 @socketio.on('reset')
 #FIXME: make sure reset signal send from one of connected players
 def reset_game():
-    if request.sid not in clients.keys():
+    if request.sid not in clients.values():
         return
     deck_tops = [{'color': 'empty', 'number': 'empty'}, {'color': 'empty', 'number': 'empty'}]
     cards = [card for card in total_cards]
@@ -115,7 +116,7 @@ def reset_game():
 @socketio.on('place')
 def changeTops(element):
     elements = element
-    emit('tops-change', elements)
+    emit('tops-change', elements, broadcast = True)
 
 if __name__ == '__main__':
     socketio.run(app)
